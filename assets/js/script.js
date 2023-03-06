ae79f3aa272229a3840059f8e9a22d4cc49234fd
a.breezy <ambrose.wilkins@gmail.com>
2023-03-06 11:40:17 -0500

    google map is created based on yelp data and restaurantData is appended to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-var results = $("#result");
+var restaurantData = $("#restaurant-data");
 var favorite = [];
 var tempObj;
 let map;
@@ -29,15 +29,12 @@ function yelpAPI() {
 				price: randomBusiness.price,
 				rating: randomBusiness.rating,
 				url: randomBusiness.url,
+				categories: randomBusiness.categories,
 			};
-			console.log(randomBusiness);
-			console.log(businessData);
-
-			// display map here
+			tempObj = businessData;
 			DisplayData(businessData);
-			// initMap(businessLocation);
 		})
-		.catch((err) => console.error("html", err));
+		.catch((err) => console.error(err));
 }
 
 function saveLocalstorage() {
@@ -53,72 +50,48 @@ function getStorage() {
 }
 
 function DisplayData(businessData) {
-	console.log("here");
-	let { businessLocation, name, address, rating, url } = buisnessData;
-	results.html("");
+	let { businessLocation, name, address, rating, price, url, categories } =
+		businessData;
+	restaurantData.html("");
 
-	console.log(
-		"coordinates",
-		businessLocation,
-		"name",
-		name,
-		"address",
-		address,
-		"rating",
-		rating,
-		"url",
-		url
-	);
-	// use data from yelp to create map
-	initMap(businessLocation.latitude, businessLocation.longitude);
+	initMap(businessLocation);
 
-	// set url and rating to data from yelp
-	// var url, rating;
-	// url = data.url;
-	// rating = data.rating;
+	let restDataEl = document.createElement("article");
+	restDataEl.className = "box result-spot pb-2";
 
-	var resultTileEl = document.createElement("article");
-	resultTileEl.className = "box result-spot pb-2";
-
-	// create element and append data from yelp to it
-	var nameEl = document.createElement("a");
+	let nameEl = document.createElement("a");
 	nameEl.href = url;
 	nameEl.target = "_blank";
-	// nameEl.textContent = data.name;
+	nameEl.textContent = name;
 	nameEl.className = "subtitle block";
+	console.log(nameEl);
 
-	// create element to house address info
+	// create element for address
 	var addressEl = document.createElement("address");
-	addressEl.innerHTML =
-		data.location.address1 +
-		" </br>" +
-		data.location.city +
-		", " +
-		data.location.state +
-		" " +
-		data.location.zip_code +
-		" " +
-		data.location.country +
-		" </br>P:" +
-		data.phone.replace("+", "");
+	addressEl.innerHTML = address[0] + " </br>" + address[1];
 	addressEl.className = "content pt-4";
+	console.log(addressEl);
 
-	// create element to house categories
+	// create element for categories
 	var categoryStr = "";
 	var categoryEl = document.createElement("p");
-	for (x in data.categories) {
-		categoryStr += data.categories[x].title + "/";
+	categoryEl.className = "content";
+	for (x in categories) {
+		if (x < 2) {
+			categoryStr += categories[x].title + " · ";
+		} else {
+			categoryStr += categories[x].title;
+		}
 	}
 	categoryEl.textContent = "Category: " + categoryStr.slice(0, -1);
-	categoryEl.className = "content";
 
-	// create element to house price of resto
-	var moneyEl = document.createElement("p");
-	moneyEl.className = "content";
-	if (data.price === null || data.price === "" || data.price === undefined) {
-		moneyEl.textContent = "Price: Not Available";
+	// create element for price
+	var priceEl = document.createElement("p");
+	priceEl.className = "content";
+	if (price === null || price === "" || price === undefined) {
+		priceEl.textContent = "Price: Not Available";
 	} else {
-		moneyEl.textContent = "Price: " + data.price;
+		priceEl.textContent = "Price: " + price;
 	}
 
 	// add in a favorite button
@@ -129,22 +102,18 @@ function DisplayData(businessData) {
 		"<spas class='far fa-star mr-2'></span>Save for later";
 
 	// append the results of yelp search
-	// then append above data to that container
-	results.append(resultTileEl);
-	resultTileEl.append(nameEl, addressEl, categoryEl, moneyEl, favoriteButtonEl);
+	restaurantData.append(restDataEl);
+	restDataEl.append(nameEl, addressEl, categoryEl, priceEl, favoriteButtonEl);
 }
 
 // init a google map to put in the above function
 function initMap(businessLocation) {
-	var options = {
+	map = new google.maps.Map(document.getElementById("map"), {
 		center: businessLocation,
 		zoom: 18,
-	};
+	});
 
-	// create new Map instance
-	map = new google.maps.Map(document.getElementById("map"), options);
-
-	// not sure what this does, but I think it's some sort of jquery
+	// append map instance to map id on page
 	$("#map").addClass("box");
 
 	// put a marker on the map
@@ -156,7 +125,7 @@ function initMap(businessLocation) {
 
 function saveFavorite() {
 	favorite.push(tempObj);
-	console.log(favorite);
+	// console.log(favorite);
 	saveLocalstorage();
 }
 
